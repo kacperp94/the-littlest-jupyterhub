b9b09f5c09a99d95a79a7569a62554a86116eb9e
Kacper Piotrowski <kacperp94@users.noreply.github.com>
2019-05-13 13:00:40 -0500

    Update bootstrap.py

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -86,7 +86,7 @@ def main():
         'git+https://github.com/jupyterhub/the-littlest-jupyterhub.git'
     )
 
-    subprocess.check_output('pip3 install' + pip_flags + [tljh_repo_path], stderr=subprocess.STDOUT)
+    subprocess.check_output('pip3 install' + pip_flags + tljh_repo_path, stderr=subprocess.STDOUT)
     logger.info('Setup tljh package')
 
     logger.info('Starting TLJH installer...')
